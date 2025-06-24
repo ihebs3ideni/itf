fc1fafc06a44e3a32b2957c2931b703c0c315c33
Dragan Bjedov <dragan.db.bjedov@bmw.de>
2025-06-24 16:14:02 +0200

    Fixed lint issues in docker.py

--- itf/plugins/docker.py
+++ itf/plugins/docker.py
@@ -38,15 +38,11 @@ def pytest_addoption(parser):
 def docker(request):
     docker_image_bootstrap = request.config.getoption("docker_image_bootstrap")
     if docker_image_bootstrap:
-        logger.info(
-            f"Executing custom image bootstrap command: {docker_image_bootstrap}"
-        )
+        logger.info(f"Executing custom image bootstrap command: {docker_image_bootstrap}")
         subprocess.run([docker_image_bootstrap], check=True)
 
     docker_image = request.config.getoption("docker_image")
     client = pypi_docker.from_env()
-    container = client.containers.run(
-        docker_image, "sleep infinity", detach=True, auto_remove=True, init=True
-    )
+    container = client.containers.run(docker_image, "sleep infinity", detach=True, auto_remove=True, init=True)
     yield container
     container.stop(timeout=1)
