ca6c1d7a553209d42f422cee107303ded4b73ca0
Dragan Bjedov <dragan.db.bjedov@bmw.de>
2025-09-16 17:25:32 +0200

    Save ITF results in JUnit xml file itf-results.xml

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+import os
 import sys
 import pytest
+import itf.plugins.utils.bazel as bazel
 
 
 if __name__ == "__main__":
     args = sys.argv[1:]
+    args += [f"--junitxml={os.path.join(bazel.get_output_dir(), 'itf-results.xml')}"]
     sys.exit(pytest.main(args))
